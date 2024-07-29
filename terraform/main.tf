30a09ee8c44e79f3e40e2115e1893fc8cb9bab47
pashtunzalmay <pashtunzalmay@pashtunzalmay-HP-Laptop-17-cn1xxx>
2024-07-29 07:03:47 -0400

    updated jenkinsfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -172,7 +172,7 @@ resource "aws_lambda_function" "todo_lambda" {
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.12"
   timeout          = 30
-  source_code_hash = filebase64sha256("../package/lambda_function.zip")
+  source_code_hash = filebase64sha256("lambda_function.zip")
   filename         = var.lambda_zip_file
 
   environment {
