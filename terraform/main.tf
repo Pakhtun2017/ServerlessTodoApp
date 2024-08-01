0e7a74d2d9aaffe5c857bf84e95ae3c7ca4a0dd7
pashtunzalmay <pashtunzalmay@pashtunzalmay-HP-Laptop-17-cn1xxx>
2024-08-01 15:17:14 -0400

    updated api gateway stage in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -291,7 +291,7 @@ resource "aws_api_gateway_domain_name" "todo_domain" {
 resource "aws_api_gateway_base_path_mapping" "todo_base_path_mapping" {
   count       = local.api_stage_exists ? 0 : 1
   api_id      = aws_api_gateway_rest_api.todo_api[0].id
-  stage_name  = aws_api_gateway_stage.todo_stage.stage_name
+  stage_name  = aws_api_gateway_stage.todo_stage[0].stage_name
   domain_name = aws_api_gateway_domain_name.todo_domain[0].domain_name
 }
 
